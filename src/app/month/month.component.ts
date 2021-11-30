c68a4166868f35cb49d1782b22f9885364294ffb
David Obodo <obododavid5@gmail.com>
2021-11-30 05:00:57 +0100

    feature: create first component

--- src/app/month/month.component.ts
+++ src/app/month/month.component.ts
@@ -3,13 +3,44 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-month',
   templateUrl: './month.component.html',
-  styleUrls: ['./month.component.sass']
+  styleUrls: ['./month.component.sass'],
 })
 export class MonthComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  days: number[];
+  constructor() {
+    this.days = [
+      1,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      10,
+      11,
+      12,
+      13,
+      14,
+      15,
+      16,
+      17,
+      18,
+      19,
+      20,
+      21,
+      22,
+      23,
+      24,
+      25,
+      26,
+      27,
+      28,
+      29,
+      30,
+    ];
   }
 
+  ngOnInit(): void {}
 }
