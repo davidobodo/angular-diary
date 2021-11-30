3a6a38258e2570f403226fc736831bd9c8037eff
David Obodo <obododavid5@gmail.com>
2021-11-30 04:24:15 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MonthComponent } from './month/month.component';
+import { HourComponent } from './hour/hour.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonthComponent,
+    HourComponent
   ],
   imports: [
     BrowserModule,
